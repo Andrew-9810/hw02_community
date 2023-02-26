005382b57d100413fea527da98a7a017d7bc2679
AndreyGolovanov <andrew-9810@yandex.ru>
2023-02-26 10:13:59 +1000

    lesson 4/16 2/6 8/9

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     # Добавляем к путям из пиложения posts пространство имен.
-    path('', include('posts.urls', namespace='posts_namespace')),
+    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
