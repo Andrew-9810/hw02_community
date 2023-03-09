3c71b36baee0e4303aa56df9b0efde382d179ba2
AndreyGolovanov <andrew-9810@yandex.ru>
2023-03-09 22:35:19 +1000

    lesson 4/14 4/6 /

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 from users.views import authorized_only
+from django.core.paginator import Paginator
 # from django.contrib.auth.decorators import login_required
 SHOW_QUANTITY: int = 10
 
@@ -8,9 +9,12 @@ SHOW_QUANTITY: int = 10
 def index(request):
     """Главная страница."""
     template = 'posts/index.html'
-    posts = Post.objects.order_by()[:SHOW_QUANTITY]
+    posts = Post.objects.order_by()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts
+        'page_obj': page_obj,
     }
     return render(request, template, context)
 
@@ -22,9 +26,12 @@ def group_posts(request, slug):
     """Страница постов отсортированных по группам."""
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = (group.posts_group.all()[:SHOW_QUANTITY])
+    posts = (group.posts_group.all())
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
